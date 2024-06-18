b20a93c80d999b4507a0de08765c28148beb7bfb
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-18 17:36:25 +0600

    categories section data added

--- index.js
+++ index.js
@@ -4,11 +4,56 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+
 // middleware 
 app.use(cors());
 app.use(express.json());
-// ShoppingZone
-// SgM2JANdl7nZyuzW
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzichn4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const CategoriesCollection = client.db('ShoppingZone').collection('Categories');
+
+    // -------------------------------- Categories----------------------
+
+    app.get("/categories", async(req,res)=>{
+        const result = await CategoriesCollection.find().toArray();
+        res.send(result);
+    })
+
+
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 
 app.get('/',(req,res)=>{
     res.send('Shopping Server Running Successfully');
