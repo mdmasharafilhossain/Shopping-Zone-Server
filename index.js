9cf016973cb4bb3d12aa324254d636559061d6cc
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2024-06-20 11:19:24 +0600

    flashSale Collection added

--- index.js
+++ index.js
@@ -25,19 +25,24 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // ----------------------------All Collection Name------------------------
     const CategoriesCollection = client.db('ShoppingZone').collection('Categories');
+    const FlashSaleCollection = client.db('ShoppingZone').collection('FlashSale');
 
     // -------------------------------- Categories----------------------
 
     app.get("/categories", async(req,res)=>{
         const result = await CategoriesCollection.find().toArray();
-        res.send(result);
+        res.send({result});
     })
+
+
+    // ----------------------------Flash Sale-----------------------------------
+    app.get("/flashSale", async(req,res)=>{
+      const result = await FlashSaleCollection.find().toArray();
+      res.send({result});
+  })
+
 
 
 
